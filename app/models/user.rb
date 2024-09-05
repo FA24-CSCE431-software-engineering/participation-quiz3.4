6d7a09dde00a694410bb207221e8925243dd0fd7
Kevin Johnson <john2003.kjj@gmail.com>
2024-09-05 22:08:58 +0000

    tests now passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-#  has_many :posts
-#  has_many :comments
+    validates_presence_of :first_name
+    validates_presence_of :last_name
+
+    has_many :posts
+    has_many :comments
 #add another comment. Yeah
 
 
